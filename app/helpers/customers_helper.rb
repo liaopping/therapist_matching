49aff2f087bff93795a7299e35dc3acc78e8cb27
liaopping <race.lifes.a.race@gmail.com>
2020-09-17 08:55:21 +0900

    [Add]カスタマーヘルパーメソッド定義・ビューでの呼び出し_#5

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,7 @@
 module CustomersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(@customer.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: @customer.name, class: "gravatar")
+  end
 end
